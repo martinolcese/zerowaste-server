349b4223adb261a6519ee7d8ad1fe94dba19d904
martinolcese <valdes10olcese@live.com>
2021-11-11 23:17:07 -0300

    Update TipsModel.js
    update on tip

--- model/TipsModel.js
+++ model/TipsModel.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TipsSchema = new Schema({
-  code: {
-    type: String,
-    required: true,
-    index: { unique: true }
-  },
+  
   title: {
     type: String,
     required: true,
   },
   description: {
+    type: String,
+    required: true,
+  },
+  color: {
     type: String,
     required: true,
   },
